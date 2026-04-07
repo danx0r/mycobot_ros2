712700fb196ac3f03a2f58a36795a1b0b9429295
danx0r <elmer@fudd.edu>
2026-04-06 23:34:01 -0700

    something

--- apt_install.sh
+++ apt_install.sh
@@ -1,3 +1,4 @@
 apt install ros-humble-moveit 
 apt install ros-humble-ros2-control ros-humble-ros2-controllers
 apt install ros-humble-gazebo-ros-pkgs ros-humble-gazebo-ros2-control
+apt install assimp-utils
